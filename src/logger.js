eceb635dccf290168fc553c29d428c64f5105de4
Patrick Kalita <patrick.kalita@gmail.com>
2019-04-24 21:31:08 -0700

    change log file location

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,10 @@
+import path from 'path';
 import winston from 'winston';
 
 winston.configure({
   transports: [
     new winston.transports.File({
-      filename: 'playball.log',
+      filename: path.resolve(__dirname, 'playball.log'),
       format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
