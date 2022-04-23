a15a3ca4f1aabb0c079dcbe1038c623c97273c14
Patrick Kalita <patrick.kalita@gmail.com>
2022-04-22 18:05:32 -0700

    Imrpove play result list formatting

--- src/components/AllPlays.jsx
+++ src/components/AllPlays.jsx
@@ -4,9 +4,31 @@ import { selectAllPlays, selectTeams } from '../features/games';
 
 import style from '../style';
 
+
+function getPlayResultColor(play) {
+  const lastPlay = play.playEvents[play.playEvents.length - 1].details;
+  if (lastPlay.isBall) {
+    return 'green';
+  } else if (lastPlay.isStrike) {
+    return 'red';
+  } else if (lastPlay.isInPlay && lastPlay.description.indexOf(', out') < 0) {
+    return 'blue';
+  } else {
+    return 'white';
+  }
+}
+
+
 function AllPlays() {
   const plays = useSelector(selectAllPlays);
   const teams = useSelector(selectTeams);
+
+  const formatScoreDetail = (scoreObj) => (
+    ' {bold}{white-bg}{black-fg} ' + 
+    `${teams.away.abbreviation} ${scoreObj.awayScore} - ` +
+    `${teams.home.abbreviation} ${scoreObj.homeScore}` + 
+    ' {/black-fg}{/white-bg}{/bold}'
+  );
 
   let inning = '';
   const lines = [];
@@ -14,21 +36,24 @@ function AllPlays() {
     const playInning = play.about.halfInning + ' ' + play.about.inning;
     if (playInning !== inning) {
       inning = playInning;
-      lines.push(`{yellow-fg}[${inning.toUpperCase()}]{/yellow-fg}`);
+      if (lines.length > 0) {
+        lines.push('');
+      }
+      lines.push(`{bold}[${inning.toUpperCase()}]{/bold}`);
     }
+
     if (play.about.isComplete) {
-      let line = `[${play.result.event}] ${play.result.description}`;
+      const color = getPlayResultColor(play);
+      let line = `{${color}-fg}[${play.result.event}]{/${color}-fg} ${play.result.description}`;
       if (play.about.hasOut) {
         line += ` {bold}${play.count.outs} out{/bold}`;
       }
       if (play.about.isScoringPlay) {
-        line += ' {bold}{white-bg}{black-fg} ' + 
-          `${teams.away.abbreviation} ${play.result.awayScore} - ` +
-          `${teams.home.abbreviation} ${play.result.homeScore}` + 
-          ' {/black-fg}{/white-bg}{/bold}';
+        line += formatScoreDetail(play.result);
       }
       lines.push(line);
     }
+
     play.playEvents && play.playEvents.slice().reverse().forEach(event => {
       if (event.type === 'action') {
         let line = '';
@@ -37,10 +62,7 @@ function AllPlays() {
         }
         line += event.details.description;
         if (event.isScoringPlay) {
-          line += ' {white-bg}{black-fg}{bold}' +
-            `${teams.away.abbreviation} ${event.details.awayScore} - ` + 
-            `${teams.home.abbreviation} ${event.details.homeScore}` + 
-            '{/bold}{/black-fg}{/white-bg}';
+          line += formatScoreDetail(event.details);
         }
         lines.push(line);
       }
