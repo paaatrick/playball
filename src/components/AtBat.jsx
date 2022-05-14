f42c8f5da8d56a0bde3186340603432cdcd1311e
Patrick Kalita <patrick.kalita@gmail.com>
2022-05-14 23:50:12 +0100

    Move play result to top of at bat display

--- src/components/AtBat.jsx
+++ src/components/AtBat.jsx
@@ -6,27 +6,33 @@ function AtBat() {
   const currentPlay = useSelector(selectCurrentPlay);
   const playEvents = currentPlay.playEvents;
   const playResult = currentPlay.about.isComplete ? currentPlay.result.description : '';
-  const content = playEvents && playEvents.slice().reverse().map(event => {
-    let line = '';
-    if (event.isPitch) {
-      line = `[${event.details.description}] `;
-      if (event.pitchData?.startSpeed) {
-        line += `${event.pitchData.startSpeed} MPH `;
+  let content = '';
+  if (playResult) {
+    content += `${playResult}\n\n`;
+  } 
+  if (playEvents && playEvents.length) {
+    content += playEvents.slice().reverse().map(event => {
+      let line = '';
+      if (event.isPitch) {
+        line = `[${event.details.description}] `;
+        if (event.pitchData?.startSpeed) {
+          line += `${event.pitchData.startSpeed} MPH `;
+        }
+        if (event.details?.type?.description) {
+          line += event.details.type.description;
+        }
+        if (!event.details?.isInPlay) {
+          line += `{|} ${event.count.balls}-${event.count.strikes}`;
+        }
+      } else {
+        if (event.details?.event) {
+          line += `[${event.details.event}] `;
+        }
+        line += event.details.description;
       }
-      if (event.details?.type?.description) {
-        line += event.details.type.description;
-      }
-      if (!event.details?.isInPlay) {
-        line += `{|} ${event.count.balls}-${event.count.strikes}`;
-      }
-    } else {
-      if (event.details?.event) {
-        line += `[${event.details.event}] `;
-      }
-      line += event.details.description;
-    }
-    return line;
-  }).join('\n') + `\n\n${playResult}`;
+      return line;
+    }).join('\n');
+  }
   return (
     <box content={content} tags />
   );
