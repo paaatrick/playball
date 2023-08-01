db3bcf5f56c0a97ab06afcb83619c9bb3e54a063
Patrick Kalita <patrick.kalita@gmail.com>
2023-07-31 20:14:41 -0700

    Add update notification

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,25 @@
 /* eslint no-console: 0 */
 import { Command } from 'commander';
+import updateNotifier from 'update-notifier';
 
 import * as config from './config.js';
 import main from './main.js';
 import pkg from './package.js';
 
 const program = new Command();
+const notifier = updateNotifier({
+  pkg, 
+  updateCheckInterval: 1000 * 60 * 60 * 24 * 7  // 1 week
+});
 
 program
   .name(pkg.name)
   .description(pkg.description)
   .version(pkg.version)
-  .action(main);
+  .action(main)
+  .hook('postAction', () => notifier.notify({
+    isGlobal: true,
+  }));
 
 program.command('config')
   .description('Set or get configration values')
