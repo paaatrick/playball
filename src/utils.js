247172e65b921593edb0cb3b3a1897e746c6e684
Patrick Kalita <3904051+paaatrick@users.noreply.github.com>
2024-03-29 08:46:47 -0700

    Fix favorite star resetting all styles in schedule line

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,9 @@ import { get } from './config.js';
 const FAVORITES = get('favorites');
 
 export function teamFavoriteStar(team) {
+  const style = get('color.favorite-star') + '-fg'
   if (FAVORITES.includes(team.abbreviation)) {
-    return `{${get('color.favorite-star')}-fg}★{/} `;
+    return `{${style}}★{/${style}} `;
   }
   return '';
 }
