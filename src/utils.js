fea31da23dac731ae98f661ab752c3ffa7dc30ca
Patrick Kalita <3904051+paaatrick@users.noreply.github.com>
2024-03-29 08:47:55 -0700

    Add missing semicolon

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import { get } from './config.js';
 const FAVORITES = get('favorites');
 
 export function teamFavoriteStar(team) {
-  const style = get('color.favorite-star') + '-fg'
+  const style = get('color.favorite-star') + '-fg';
   if (FAVORITES.includes(team.abbreviation)) {
     return `{${style}}★{/${style}} `;
   }
