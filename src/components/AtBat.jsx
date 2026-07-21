22abb171306f295bd14a7f1419c75e9a93541c86
Patrick Kalita <3904051+paaatrick@users.noreply.github.com>
2026-07-21 12:01:48 -0700

    Show status of ABS challenged pitches in at bat display

--- src/components/AtBat.jsx
+++ src/components/AtBat.jsx
@@ -6,15 +6,38 @@ function AtBat() {
   const currentPlay = useSelector(selectCurrentPlay);
   const playEvents = currentPlay.playEvents;
   const playResult = currentPlay.about.isComplete ? currentPlay.result.description : '';
+
+  function formatEventReview(event, idx, allEvents) {
+    let reviewDetails = undefined;
+    if (event.reviewDetails) {
+      // If the event has review details, we can use them directly
+      reviewDetails = event.reviewDetails;
+    } else {
+      // If the event doesn't have review details, check if this is the last pitch event and then
+      // check the current play's review details
+      if (event.isPitch && allEvents.slice(0, idx).every((e) => !e.isPitch)) {
+        reviewDetails = currentPlay.reviewDetails;
+      }
+    }
+    if (reviewDetails) {
+      if (reviewDetails.inProgress) {
+        return ' - Challenged';
+      } else if (reviewDetails.isOverturned !== undefined) {
+        return reviewDetails.isOverturned ? ' - Overturned' : ' - Upheld';
+      }
+    }
+    return '';
+  }
+
   let content = '';
   if (playResult) {
     content += `${playResult}\n\n`;
   } 
   if (playEvents && playEvents.length) {
-    content += playEvents.slice().reverse().map(event => {
+    content += playEvents.slice().reverse().map((event, idx, allEvents) => {
       let line = '';
       if (event.isPitch) {
-        line = `[${event.details.description}] `;
+        line = `[${event.details.description}${formatEventReview(event, idx, allEvents)}] `;
         if (event.pitchData?.startSpeed) {
           line += `${event.pitchData.startSpeed} MPH `;
         }
